c1bf3ac4532a3612c1fc21f64f5a283cdbc8f1f1
melissaintrees <melissaintrees@gmail.com>
2018-12-29 18:39:13 -0600

    Updates

---
+++ precache-manifest.db853e801966b5813e351d92ad42bb83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cdd8992958229eeb81a",
+    "url": "/React-Crystal-Memory-Game/static/js/runtime~main.1cdd8992.js"
+  },
+  {
+    "revision": "e5c7b4cca371b29fb457",
+    "url": "/React-Crystal-Memory-Game/static/js/main.e5c7b4cc.chunk.js"
+  },
+  {
+    "revision": "5cabbdc29baec113d4ea",
+    "url": "/React-Crystal-Memory-Game/static/js/1.5cabbdc2.chunk.js"
+  },
+  {
+    "revision": "e5c7b4cca371b29fb457",
+    "url": "/React-Crystal-Memory-Game/static/css/main.d30db6ff.chunk.css"
+  },
+  {
+    "revision": "93e92846dd532fa2c81ff88efc885218",
+    "url": "/React-Crystal-Memory-Game/index.html"
+  }
+];
